3dea9682f8dc2a51ad0da39c4e52d7d840a8d79a
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-07-03 22:52:24 -0300

    backend updated

---
+++ api/src/controllers/getAllPokemons.js
@@ -0,0 +1,17 @@
+//en este componente traigo los pokemones para mostrar desde la api y la base de datos
+//y asi renderizarlo en la home page del front
+
+const getApiData = require('./getApiData');
+const getPokemonsDB = require('./getPokemonsDB');
+
+const getAllPokemons = async () => {
+    const apiPokemons = await getApiData(100); //obtengo pokemones de la api
+    DBPokemons = await getPokemonsDB(); // obtengo pokemones de la base de datos
+    // if (DBPokemons.length === 0) throw new Error('No se encontraron pokemones en la base de datos.');
+    const allPokemons = [...apiPokemons, ...DBPokemons]; // copia de los pokemones de la api y bd en un variable para todos
+
+    if (allPokemons.length === 0) throw new Error('No se encontraron pokemones para mostrar.'); //de no encontrar lanzo mensaje de error
+    return allPokemons; // caso contrario devuelvo todos los pokemones api y base de datos
+};
+
+module.exports = getAllPokemons;
