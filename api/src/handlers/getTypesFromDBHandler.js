b148a4bb1f896b6e6612dc3ea3c7a99f6075d0d6
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-07-11 17:39:40 -0300

    back updated

---
+++ api/src/handlers/getTypesFromDBHandler.js
@@ -0,0 +1,12 @@
+const getTypesFromDB = require('../controllers/getTypesFromDB');
+
+const getTypesFromDBHandler = async (req, res) => { 
+    try {
+        const types = await getTypesFromDB();
+        return res.status(200).json(types);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = getTypesFromDBHandler;
