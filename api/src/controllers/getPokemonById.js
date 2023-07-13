47b857115306145e0f4307b65e812715de3a60be
eenriquez.jose@gmail.com <eenriquez.jose@gmail.com>
2023-07-13 02:59:17 -0300

    back updated

--- api/src/controllers/getPokemonById.js
+++ api/src/controllers/getPokemonById.js
@@ -2,35 +2,15 @@ const { Pokemon, Type } = require('../db');
 const axios = require('axios');
 const URL = 'https://pokeapi.co/api/v2/pokemon';
 
-const getPokemonsById = async (id, source) => {
-    if (source === 'api') {
-        const response = (await axios.get(`${URL}/${id}`)).data;
-        const pokemonData = response;
+const getPokemonsById = async (id) => {
+    if(isNaN(id)){
+        const pokemonDB = await Pokemon.findByPk(id, { include: [Type] });
+        if(!pokemonDB) throw new Error(`No se encuentra el perro con el id: ${id} en la base de datos`);
 
-        const types = pokemonData.types.map((type) => type.type.name);
+        const pokemon = pokemonDB.toJSON();
+        const types = pokemon.Types.map((type) => type.name);
 
-        return {
-            name: pokemonData.name,
-            id: pokemonData.id,
-            image: pokemonData.sprites?.other?.dream_world?.front_default,
-            height: pokemonData.height,
-            weight: pokemonData.weight,
-            life: pokemonData.stats[0].base_stat,
-            attack: pokemonData.stats[1].base_stat,
-            defense: pokemonData.stats[2].base_stat,
-            speed: pokemonData.stats[5].base_stat,
-            types: types
-        };
-    } else {
-        const pokemon = await Pokemon.findByPk(id, {
-            include: {
-                model: Type,
-                attributes: ['name'],
-                through: { attributes: [] },
-            },
-        });
-
-        return {
+        return{
             id: pokemon.id,
             name: pokemon.name,
             image: pokemon.image,
@@ -40,9 +20,27 @@ const getPokemonsById = async (id, source) => {
             speed: pokemon.speed,
             height: pokemon.height,
             weight: pokemon.weight,
-            types: pokemon.Types.map((type) => type.name),
+            types: types
         };
     }
+
+    const pokemonApi = (await axios.get(`${URL}/${id}`)).data;
+    if(!pokemonApi) throw new Error(`No se encuentra el perro con el id: ${id} en la api`);
+
+    const types = pokemonApi.types.map((type) => type.type.name);
+
+    return{
+        name: pokemonApi.name,
+        id: pokemonApi.id,
+        image: pokemonApi.sprites?.other?.dream_world?.front_default,
+        height: pokemonApi.height,
+        weight: pokemonApi.weight,
+        life: pokemonApi.stats[0].base_stat,
+        attack: pokemonApi.stats[1].base_stat,
+        defense: pokemonApi.stats[2].base_stat,
+        speed: pokemonApi.stats[5].base_stat,
+        types: types
+    };
 };
 
 module.exports = getPokemonsById;
--- api/src/handlers/getPokemonsByIdHandler.js
+++ api/src/handlers/getPokemonsByIdHandler.js
@@ -2,10 +2,8 @@ const getPokemonsById = require('../controllers/getPokemonById');
 
 const getPokemonsByIdHandler = async (req, res) => {
     const { id } = req.params;
-    const source = isNaN(id) ? 'bd' : 'api';
-
     try {
-        const response = await getPokemonsById(id, source);
+        const response = await getPokemonsById(id);
         return res.status(200).send(response);
     } catch (error) {
         return res.status(400).json({ error: error.message });
