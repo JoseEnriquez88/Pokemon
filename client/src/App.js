ee22b1ca5ff76607181ae272e951c82a927fc555
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-10-30 16:38:20 -0300

    restructuring code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import axios from 'axios';
 import { Routes, Route } from "react-router-dom";
-
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import Create from "./components/Create/Create";
 import Footer from "./components/Footer/Footer";
 import Detail from "./components/Detail/Detail";
 import Error from "./components/Error/Error";
+
+axios.defaults.baseURL= 'http://localhost:3001/'
 
 function App() {
   return (
