fee7d05174dcf02cae8101fd0af1a11b34e2e5ac
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-11-01 14:44:31 -0300

    .

--- api/src/app.js
+++ api/src/app.js
@@ -14,17 +14,36 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pokemon-delta-ten.vercel.app");
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://pokemon-delta-ten.vercel.app");
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+server.use((req, res, next) => {
+  const allowedOrigins = [
+    "https://pokemon-delta-ten.vercel.app",
+    "http://localhost:3000",
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  }
+
   next();
 });
+
 
 server.use("/", routes);
 
