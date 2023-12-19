1c79ecfc6fb4e0d762ce94508730fef6fa1e170b
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-12-19 05:36:45 -0300

    server updated

--- api/src/app.js
+++ api/src/app.js
@@ -14,17 +14,6 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://pokemon-delta-ten.vercel.app");
-// res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
