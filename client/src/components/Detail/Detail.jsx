bcf30cb4be5e5888dd69324faeda582936d9ec99
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-11-04 01:28:11 -0300

    detail  component was updated

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
 import style from "./detail.module.css";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemonById, cleanDetail } from "../../redux/actions";
